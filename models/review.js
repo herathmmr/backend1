322c303b1a0de1c2139e98e88bf5cd06a2225915
Malshan Herath <mmrherath@students.nsbm.ac.lk>
2025-08-11 01:56:46 +0530

    create a model for review

---
+++ models/review.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        required :true,
+        unique : true
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    Comment :{
+        type: String,
+        required : true
+    },
+    date : {
+        type : Date,
+        required : true,
+        default : Date.now
+    }
+     
+});
+
+const Review = mongoose.model("Review",reviewSchema);
+
+export default Review;
