4e048f9460374bcaa33bcf24866ddf07f5e7f284
Malshan Herath <mmrherath@students.nsbm.ac.lk>
2025-08-11 02:10:34 +0530

    create reviewController and update review.js

---
+++ controllers/reviewController.js
@@ -0,0 +1,10 @@
+import Review from "../models/review.js";
+
+export function addReview(req,res){
+    if(user == null){
+        res.state(401).json({
+            message : "please login and try again"
+        })
+    }
+    return
+}
