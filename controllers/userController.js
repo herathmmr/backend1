7b06f6914fd056589d63b351e5ed9c4d627288ad
Malshan Herath <mmrherath@students.nsbm.ac.lk>
2025-10-31 20:34:05 +0530

    comment 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,3 +74,10 @@ if(req.user != null && req.user.role == "customer"){
     return isCustomer = true;
 }return isCustomer;}
 
+export function isItCustomer(req){
+
+/*let isCustomer = false;
+if(req.user != null && req.user.role == "customer"){
+    return isCustomer = true;
+}return isCustomer;}
+*/
